aca733664779378a454070db7fc8f6f127b2e055
hj.tian <hj.tian@aftership.com>
2021-11-17 13:20:51 +0800

    feat: add three sum double point

--- code/three_sum_5.py
+++ code/three_sum_5.py
@@ -26,12 +26,43 @@ def three_sum_map(nums: list) -> list:
 
 
 def three_sum_double_point(nums: list) -> list:
-    # TODO 使用双指针
-    ...
+    # 使用双指针 => O(n*n)
+    nums.sort()
+    length = len(nums)
+    result = []
+    for i in range(0, length):
+        j = i + 1
+        k = length - 1
+        while j < k:
+            total = nums[i] + nums[j] + nums[k]
+            if total > 0:
+                k -= 1
+            elif total < 0:
+                j += 1
+            else:
+                if [nums[i], nums[j], nums[k]] not in result:
+                    result.append([nums[i], nums[j], nums[k]])
+                j += 1
+                k -= 1
+    return result
 
 
 if __name__ == "__main__":
+
+    assert three_sum_double_point([-1, 3, 4, 0]) == []
+    assert three_sum_double_point([-1, 2, 1, 0]) == [[-1, 0, 1]]
+    assert three_sum_double_point([-1, 2, -1, 0]) == [[-1, -1, 2]]
+    assert three_sum_double_point([-1, 2, -1, -1, -1, -1]) == [[-1, -1, 2]]
+    assert three_sum_double_point([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+
+    assert three_sum_force([-1, 3, 4, 0]) == []
     assert three_sum_force([-1, 2, 1, 0]) == [[-1, 0, 1]]
+    assert three_sum_force([-1, 2, -1]) == [[-1, -1, 2]]
+    assert three_sum_force([-1, 2, -1, -1, -1, -1]) == [[-1, -1, 2]]
     assert three_sum_force([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+
+    assert three_sum_map([-1, 3, 4, 0]) == []
     assert three_sum_map([-1, 2, 1, 0]) == [[-1, 0, 1]]
+    assert three_sum_map([-1, 2, -1, 0]) == [[-1, -1, 2]]
+    assert three_sum_map([-1, 2, -1, -1, -1, -1]) == [[-1, -1, 2]]
     assert three_sum_map([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
