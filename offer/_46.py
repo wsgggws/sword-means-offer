836f3c5cbdef06cd58eb36bc4227213d5867ae48
hj.tian <hj.tian@aftership.com>
2021-11-28 00:31:07 +0800

    fix: update offer _46
    update offer _46

--- offer/_46.py
+++ offer/_46.py
@@ -15,18 +15,28 @@
 
 class Solution:
     def translateNum(self, num: int) -> int:
-        text = "0" + str(num)
-        dp = []
-        # dp[n] 表示前 n 个字符所能实现的翻译方法
-        # 则 dp[n] = dp[n-1]，当最后两位不能用 j-z 表示，即最后一位只能依附在 dp[n-1] 种的最后面
-        #          = dp[n-1] + dp[n-2], 当最后两位能使用 j-z 表示，则可有最后两位依附在 dp[n-2] 种的后面 + 最后一位依附在 dp[n-1] 种的后面
-        dp[0], dp[1] = 1, 1
-        for i in range(2, len(text)):
-            if 10 <= int(text[i - 1 : i + 1]) <= 25:
-                dp[i] = dp[i - 1] + dp[i - 2]
+        """
+        12 + (25) =>
+            1 2 (25)
+            12 (25)
+        122 + (5) =>
+            1 2 2 (5)
+            12 2  (5)
+            1 22  (5)
+        dp[n]: n 个数字所能翻译方法的个数
+        =》 dp[n] = dp[n-1] + dp[n-2] # 最后面两个字符 <=25, >=10
+                  = dp[n-1] # 否则
+        """
+        text = str(num)
+        dp = [1 for _ in range(len(text) + 1)]
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(text) + 1):
+            if 10 <= int(text[i - 2 : i]) <= 25:
+                dp[i] = dp[i - 2] + dp[i - 1]
             else:
                 dp[i] = dp[i - 1]
-        return dp[len(text) - 1]
+        return dp[len(text)]
 
 
 if __name__ == "__main__":
